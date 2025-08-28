927dfaf62dc490930302968b2eab0d07303ab7b3
Adejumo Adedayo <adejumoadedayo350@gmail.com>
2025-08-28 16:53:41 +0100

    change the project's name

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     return (
         <nav className="navbar">
             <Link to="/">
-                <p className="text-2xl font-bold text-gradient">RESUMIND</p>
+                <p className="text-2xl font-bold text-gradient">Resunate</p>
             </Link>
             {auth.isAuthenticated ? (
                 <div className="flex gap-4">
